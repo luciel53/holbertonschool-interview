84ca626a6feaf5db95738fa279d9bd3a431204fb
luciel53 <l.leroty@gmail.com>
2023-08-24 17:51:19 +0200

    fixed return new node if head is null

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -24,28 +24,34 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->next = NULL;
 
 	if (*head == NULL) /* if empty list, new_node is the first node */
-		*head = new_node;
-
-	if (number < temp->n) /* if number is < head value new_node is first node */
 	{
-		new_node->next = temp;
 		*head = new_node;
 		return (new_node);
 	}
-	/* Else browse the list */
-	while (temp->next != NULL)
+
+	else if (head != NULL)
 	{
-		/* if number > previous node and < next node, insert it */
-		if (number >= temp->n && number <= temp->next->n)
+		if (number < temp->n) /* if number is < head value new_node is first node */
 		{
-			new_node->next = temp->next;
-			temp->next = new_node;
+			new_node->next = temp;
+			*head = new_node;
 			return (new_node);
 		}
-		/* else if number > previous and next node, continue to browse */
-		else if (number > temp->n && number > temp->next->n)
+		/* Else browse the list */
+		while (temp->next != NULL)
 		{
-			temp = temp->next;
+			/* if number > previous node and < next node, insert it */
+			if (number >= temp->n && number <= temp->next->n)
+			{
+				new_node->next = temp->next;
+				temp->next = new_node;
+				return (new_node);
+			}
+			/* else if number > previous and next node, continue to browse */
+			else if (number > temp->n && number > temp->next->n)
+			{
+				temp = temp->next;
+			}
 		}
 	}
 	temp->next = new_node; /* to insert the node at the end of the list */
