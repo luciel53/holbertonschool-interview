4d6dc71b408310a2038a2309dcf54cfb8b676342
luciel53 <l.leroty@gmail.com>
2023-08-30 11:55:41 +0200

    add if new node is null

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -6,6 +6,7 @@
 	* binary_tree_node - function that creates a binary tree node
 	* @parent: is a pointer to the parent node of the node to create
 	* @value: is the value to put in the new node
+	* Return: the new node
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
@@ -13,6 +14,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return NULL;
+	}
 
 	new_node->n = value;
 	new_node->left = NULL;
