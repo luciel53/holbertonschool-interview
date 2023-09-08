3b56f3cdd73b5fec299632d0a16346de47650836
luciel53 <l.leroty@gmail.com>
2023-09-08 14:20:18 +0200

    initalise prime _number a true

--- minimum_operations/0-main.py
+++ minimum_operations/0-main.py
@@ -5,14 +5,5 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 2
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 16
+n = 2147483640
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 In a text file, there is a single character H. Your text editor can execute
 only two operations in this file: Copy All and Paste. Given a number n, write
 a method that calculates the fewest number of operations needed to result in
-exactly n H characters in the file.
+    exactly n H characters in the file.
 
     Prototype: def minOperations(n)
     Returns an integer
@@ -14,7 +14,7 @@ exactly n H characters in the file.
 def minOperations(n):
     """find if n is prime"""
     operations = 0
-    prime_number = False
+    prime_number = True
 
     if n <= 1:
         return 0
