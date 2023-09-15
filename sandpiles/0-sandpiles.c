725c14f089606c854ddf08491d8c175255997f1a
luciel53 <l.leroty@gmail.com>
2023-09-15 22:30:11 +0200

    fix

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,76 +1,136 @@
-#include <stdlib.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-#include "sandpiles.h"
-
-void print_grid(int grid[3][3])
-	{
-		int i, j;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (j)
-					printf(" ");
-				printf("%d", grid[i][j]);
-			}
-			printf("\n");
-		}
-	}
-
+# include "sandpiles.h"
 
 /**
- * sandpiles_sum - function that computes the sum of two sandpiles
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- * Return: sum of two sandpiles
- */
+* print_grid - Print a 3 x 3 grid
+* @grid: The grid to print
+**/
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+static void print_grid(int grid[3][3])
 {
 	int i, j;
-	int result_grid[3][3];
-	int not_stable = 1;
 
-	/* add the grids */
 	for (i = 0; i < 3; i++)
 	{
-		for(j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
 		{
-			result_grid[i][j] = grid1[i][j] += grid2[i][j];
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
 		}
+		printf("\n");
 	}
+}
 
-	while (not_stable)
+/**
+ * sandpile_topple - Redistributes grains in grid1, stabilizing pile in grid2
+ * @grid2: Right 3x3 grid
+ * Return: a pointer to grid2
+**/
+
+int (*sandpile_topple(int grid1[3][3], int grid2[3][3]))[3]
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
 	{
-		not_stable = 0;
-
-		/* checks if result grid is stable */
-		for(i = 0; i < 3; i++) {
-			for(j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
 			{
-				/* checks the neighbours */
-				if(result_grid[i][j] > 3)
+				if (i - 1 >= 0)
 				{
-					not_stable = 1;
-					result_grid[i][j] -= 4;
-
-					if (i - 1 >= 0)
-						result_grid[i - 1][j]++;
-					if (i + 1 < 3)
-						result_grid[i + 1][j]++;
-					if (j - 1 >= 0)
-						result_grid[i][j - 1]++;
-					if (j + 1 < 3)
-						result_grid[i][j + 1]++;
+					if (grid1[i - 1][j] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i - 1][j] += 1;
+					}
+				}
+				else
+				{
+					grid2[i][j] -= 1;
+				}
+				if (j - 1 >= 0)
+				{
+					if (grid1[i][j - 1] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i][j - 1] += 1;
+					}
+				}
+				else
+				{
+					grid2[i][j] -= 1;
+				}
+				if (i + 1 < 3)
+				{
+					if (grid1[i + 1][j] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i + 1][j] += 1;
+					}
+				}
+				else
+				{
+					grid2[i][j] -= 1;
+				}
+				if (j + 1 < 3)
+				{
+					if (grid1[i][j + 1] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i][j + 1] += 1;
+					}
+				}
+				else
+				{
+					grid2[i][j] -= 1;
 				}
 			}
 		}
-		if (not_stable)
+	}
+	return (grid2);
+}
+
+/**
+	* sandpiles_sum - sums 2 sandpiles
+	* @grid1: first grid
+	* @grid2: grid to add
+	* Return: none
+**/
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+	int print = 0;
+	int stable = 1;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid2[i][j] != 0)
+			{
+				stable = 0;
+				grid1[i][j] += grid2[i][j];
+				grid2[i][j] = 0;
+			}
+
+			if (grid1[i][j] > 3)
+			{
+				print = 1;
+			}
+		}
+	}
+
+	if (!stable)
+	{
+		if (print)
 		{
 			printf("=\n");
-			print_grid(result_grid);
+			print_grid(grid1);
 		}
-
-	};
+		sandpiles_sum(grid1, sandpile_topple(grid1, grid2));
+	}
 }
