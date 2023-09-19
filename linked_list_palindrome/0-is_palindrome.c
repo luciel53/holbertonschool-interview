f4a361af0e24a7de863a36182512b0d78be90269
luciel53 <l.leroty@gmail.com>
2023-09-19 18:32:43 +0200

    palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+
+/**
+ * to_array - function that converts a singly linked list to an array
+ * @head: the head of the list
+ * @array: the array created
+ * Return: void
+ */
+
+void to_array(listint_t **head, int *array)
+{
+	listint_t *current = *head;
+	int i = 0;
+
+	while (current != NULL)
+	{
+		array[i] = current->n;
+		current = current->next;
+		i++;
+	}
+}
+
+
+/**
+ * is_palindrome - function to check if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 or 1 if palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *node = *head;
+
+	if (node == NULL || node->next == NULL)
+		return (1);
+
+	/* create size variable to use in allocations */
+	int size = 0;
+	/* browse the list and iterate size of the future array */
+	while (node != NULL)
+	{
+		node = node->next;
+		size++;
+	}
+
+	/* allocate the array */
+	int *array = (int *)malloc(sizeof(int) * size);
+
+	/* convert the singly linked list in array and recover data with &node */
+	to_array(&node, array);
+
+	/* allocate memory for the copy */
+	int *copy = (int *)malloc(sizeof(int) * size);
+	/* copy the array */
+	memcpy(copy, array, sizeof(int) * size);
+
+	/* checks if the array is a palindrome */
+	for (int i = 0; i < size; i++)
+	{
+		/* compare the 2 arrays */
+		if (array[i] == copy[i])
+			return (1);
+		return (0);
+	}
+	return (0);
+}
