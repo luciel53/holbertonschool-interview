7f8bb0a663c2c4bd732662028dad85a614165d2e
luciel53 <l.leroty@gmail.com>
2023-12-04 15:08:31 +0100

    task 0

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,19 +1,24 @@
-const request = require('request');
+#!/usr/bin/node
 
-const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
+const request = require('request');
+const arg = process.argv[2];
+const url = (`https://swapi-api.hbtn.io/api/films/${arg}`);
+
+const charNames = (names, i = 0) => {
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    /* convert a string of characters JSON to a javascript object and print it */
+    console.log(JSON.parse(body).name);
+    /* call recursively the function charnames and increment it to pass at the next character */
+    charNames(names, i + 1);
+  });
+};
+
+/* request to the API to recover movie informations */
 request(url, function (error, response, body) {
-  if (body === undefined) {
-    reject(error);
-  } else {
-    body = JSON.parse(body);
-	body.characters.forEach(char => {
-		if (!error) {
-			const character = JSON.parse(body);
-			console.log(character.name);
-		} else {
-			console.error('Error:', error);
-		}
-	})
-  }
+  /* extract url of characters from the body json response and store it in a var char */
+  const char = JSON.parse(body).characters
+  /* call the function charNames to print the characters */
+  charNames(char);
 });
 
