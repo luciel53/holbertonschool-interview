0216da37025bf0550283d438a9dd2f4671a07da9
luciel53 <l.leroty@gmail.com>
2024-01-19 14:22:34 +0100

    task 0

---
+++ count_it/0-count.py
@@ -0,0 +1,47 @@
+import requests
+
+def count_words(subreddit, word_list, after=None, count_dict=None):
+    if count_dict is None:
+        count_dict = {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'limit': 25}
+    if after:
+        params['after'] = after
+
+    headers = {'User-Agent': 'your_user_agent'}
+
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Error accessing subreddit '{subreddit}': {response.status_code}")
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+
+    for post in posts:
+        title_words = post['data']['title'].lower().split()
+        for word in word_list:
+            if word.lower() in title_words:
+                count_dict[word.lower()] = count_dict.get(word.lower(), 0) + title_words.count(word.lower())
+
+    after = data.get('data', {}).get('after')
+    if after:
+        count_words(subreddit, word_list, after, count_dict)
+    else:
+        if not posts:
+            print(f"No matching posts in subreddit '{subreddit}'")
+        else:
+            sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
+            for keyword, count in sorted_counts:
+                print(f"{keyword}: {count}")
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        count_words(sys.argv[1], [x for x in sys.argv[2].split()])
