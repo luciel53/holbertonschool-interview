617bc6cc47be4b764d4f66bdbe936bbd3ff9b7a1
luciel53 <l.leroty@gmail.com>
2024-01-19 14:00:08 +0100

    task 0

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,44 +1,47 @@
 #include <stdio.h>
+#include "search_algos.h"
 
-/**
- * advanced_binary - Searches for a value in a sorted array of integers.
- * @array: A pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value we re search for.
- *
- * Return: The index where value is located, or -1 if not present.
- */
+/*
+* advanced_binary - search value in the array
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in the array
+* @value: the value we're searching for
+* Return: index where value is located
+*/
+
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t i, mid;
-    int result;
+	size_t i;
+	int mid;
+	int res;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array:");
+	for (i = 0; i < size - 1; i++)
+		printf(" %d,", array[i]);
+	printf(" %d\n", array[i]);
+	mid = (size + 1) / 2;
+	if (array[0] == value)
+		return (0);
 
-    mid = (size - 1) / 2;
-    if (array[mid] == value)
-    {
-        if (size == 1 || array[mid - 1] < value)
-            return mid;
-    }
+	if (array[mid - 1] >= value)
+	{
+		res = advanced_binary(array, mid, value);
+		if (res == -1)
+			return (-1);
+		return (res);
+	}
 
-    if (array[mid] < value)
-    {
-        result = advanced_binary(array + mid + 1, size - (mid + 1), value);
-        if (result == -1)
-            return -1;
-        return mid + 1 + result;
-    }
+	else
+	{
+		if (size == 1)
+			return (-1);
+		res = advanced_binary((array + mid), size - mid, value);
+		if (res == -1)
+			return (-1);
+		return (res + mid);
+	}
 
-    return advanced_binary(array, mid, value);
 }
