cd8073a8f25eba8ba70fc067b6c82e7d0fc09767
luciel53 <l.leroty@gmail.com>
2024-01-19 16:55:07 +0100

    task 0

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -33,9 +33,9 @@ typedef struct binary_tree_s heap_t;
 
 typedef struct binary_tree_s binary_tree_t;
 
-// Function prototypes go here
+
 bool binary_tree_is_empty(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
-// ...
+int heap_extract(heap_t **root);
 
 #endif /* BINARY_TREES_H */
