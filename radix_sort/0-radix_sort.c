30fb251b71d5baee32c49f3d8b88ad85f33a6760
luciel53 <l.leroty@gmail.com>
2024-03-15 15:36:04 +0100

    task 0

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -11,8 +11,9 @@
 int getMax(int *array, size_t size)
 {
 	int max = array[0];
+	size_t i;
 
-	for (size_t i = 1; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
@@ -31,23 +32,26 @@ void countSort(int *array, size_t size, int exp)
 {
 	int *output = malloc(sizeof(int) * size);
 	int count[10] = {0};
+	size_t i;
+	int j;
+
 	/* Store count of occurrences in count[] */
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
 	/* Change count[i] so that count[i] now contains */
 	/* actual position of this digit in output[] */
-	for (int i = 1; i < 10; i++)
-		count[i] += count[i - 1];
+	for (j = 1; j < 10; j++)
+		count[j] += count[j - 1];
 	/* Build the output array */
-	for (int i = size - 1; i >= 0; i--)
+	for (j = size - 1; j >= 0; j--)
 	{
-		output[count[(array[i] / exp) % 10] - 1] = array[i];
-		count[(array[i] / exp) % 10]--;
+		output[count[(array[j] / exp) % 10] - 1] = array[j];
+		count[(array[j] / exp) % 10]--;
 	}
 
 	/* Copy the output array to array[], so that array[] now */
 	/* contains sorted numbers according to the current digit */
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = output[i];
 	free(output);
 }
