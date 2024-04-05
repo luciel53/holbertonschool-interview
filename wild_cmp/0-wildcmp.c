f81ca0348187df877abc9c3f47ae82aa8c7695d7
luciel53 <l.leroty@gmail.com>
2024-04-05 23:39:59 +0200

    task 0

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * wildcmp - function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if (*s1 == '\0' && *s2 == '\0')
+        return 1;
+
+    if (*s2 == '*')
+    {
+        if (*s1 == '\0')
+            return wildcmp(s1, s2 + 1);
+        else
+            return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+    }
+
+    if (*s1 == *s2 || (*s1 != '\0' && *s2 == '?'))
+        return wildcmp(s1 + 1, s2 + 1);
+
+    return 0;
+}
