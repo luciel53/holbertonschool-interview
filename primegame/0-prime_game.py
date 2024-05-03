7080666f01ca923bfe92d043fdfc42a0dad9aee6
luciel53 <l.leroty@gmail.com>
2024-05-03 18:01:52 +0200

    start at 2

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -9,17 +9,19 @@
 
 def isWinner(x, nums):
     """ function that determines who is winner """
+    if x < 1:
+        return None
     maria_wins = 0
     for num in nums:
-        maria_winning = 1
-        numbers = list(range(1, num + 1))
+        maria_winning = 0
+        numbers = list(range(2, num + 1))
         for number in numbers:
             if maria_winning:
                 maria_winning = 0
             else:
                 maria_winning = 1
 
-            for delet in range(1, num + 1, number):
+            for delet in range(number, num + 1, number):
                 numbers.remove(delet)
         maria_wins += maria_winning
         if maria_wins >= (x/2):
--- primegame/main_0.py
+++ primegame/main_0.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""
+Main file for testing
+"""
 
 isWinner = __import__('0-prime_game').isWinner
 
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
-
+print("Winner: {}".format(isWinner(10, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
