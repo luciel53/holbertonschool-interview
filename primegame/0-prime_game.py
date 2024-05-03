0f3e5f0c7aeca27d1e59fd9c5d748999fed70f61
luciel53 <l.leroty@gmail.com>
2024-05-03 17:45:34 +0200

    simplified code

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -9,51 +9,19 @@
 
 def isWinner(x, nums):
     """ function that determines who is winner """
-    def is_prime(num):
-        """ prime numbers """
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def next_prime(start):
-        """ next prime number """
-        num = start + 1
-        while not is_prime(num):
-            num += 1
-        return num
-
-    def game_winner(n):
-        """ If n is even, Ben will win because Maria starts """
-        if n % 2 == 0:
-            return "Ben"
-        else:
+    maria_wins = 0
+    for num in nums:
+        maria_winning = 1
+        numbers = list(range(1, num + 1))
+        for number in numbers:
+            if maria_winning:
+                maria_winning = 0
+            else:
+                maria_winning = 1
+            for delet in numbers:
+                if delet % number == 0:
+                    numbers.remove(delet)
+        maria_wins += maria_winning
+        if maria_wins > (x/2):
             return "Maria"
-
-    winners = []
-    for n in nums:
-        if n == 1:
-            # If n is 1, Ben wins because there are no prime numbers to choose
-            winners.append("Ben")
-        else:
-            prime_count = 0
-            current_prime = 1
-            while current_prime <= n:
-                prime_count += 1
-                current_prime = next_prime(current_prime)
-                # If there are no more primes, the other player wins
-                if current_prime > n:
-                    winners.append(game_winner(prime_count))
-                    break
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    return "Ben"
