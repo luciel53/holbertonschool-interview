a4af2245c20954e8ec20b12235c8d34374235d5b
luciel53 <l.leroty@gmail.com>
2024-05-03 17:15:26 +0200

    task 0betty

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    Maria and Ben are playing a game. Given a set of consecutive integers
+    starting from 1 up to and including n, they take turns choosing a prime
+    number from the set and removing that number and its multiples from the
+    set. The player that cannot make a move loses the game.
+"""
+
+
 def isWinner(x, nums):
     def is_prime(num):
         if num < 2:
